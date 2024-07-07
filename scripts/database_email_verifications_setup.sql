494c4648843468ad651a133a3308c10b0e5d525b
usmanakhmedov <akhme.v@gmail.com>
2024-07-08 01:06:28 +0200

    Updated scripts

--- scripts/database_email_verifications_setup.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE email_verifications
-(
-	email      VARCHAR(255) PRIMARY KEY,
-	code       VARCHAR(6)               NOT NULL,
-	expires_at TIMESTAMP WITH TIME ZONE NOT NULL
-);
--- scripts/database_tokens_setup.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE refresh_tokens
-(
-	token      VARCHAR(255) PRIMARY KEY,
-	user_id    UUID                     NOT NULL,
-	expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
